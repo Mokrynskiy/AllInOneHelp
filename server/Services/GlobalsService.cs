4f6bda15e49027dce8af1de3e7e4db166a6ec9f9
Alex <Alex@DESKTOP-SR3F5DF>
2020-10-24 10:35:08 +0700

    Добавьте файлы проекта.

---
+++ server/Services/GlobalsService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Radzen;
+
+namespace AllInOneHelp
+{
+    public class GlobalsService
+    {
+
+    }
+}
